f3536fae0b148382a7948a9d7083e2dae8623bd7
hallokay <hallokay044@gmail.com>
2023-07-11 00:19:53 +0900

    setup / hero

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Hero } from "@/componets"
+
+export default function Home() {
+  return (
+    <main className="overflow-hidden">
+      <Hero/>
+    </main>
+  )
+}
---
+++ componets/Hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h3 className="hero__title">
+          원하는 차를 찾고, 예약하고, 렌트하고 - 빠르고 쉽게!
+        </h3>
+        
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
+      </div>
+    </div>
+  );
+}
