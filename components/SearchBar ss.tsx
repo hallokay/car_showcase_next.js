342a532e8073ed545f5e6abf33889ec717a15398
kay <hallokay044@gmail.com>
2023-10-19 22:06:22 +0900

    수정

--- components/SearchBar ss.tsx
+++ components/SearchBar ss.tsx
@@ -1,70 +1,70 @@
-"use client";
-import { SearchManufacturer } from "./index";
-import { FormEvent, useState } from "react";
-import { useRouter } from "next/navigation";
-import Image from "next/image";
+// "use client";
+// import { SearchManufacturer } from "./index";
+// import { FormEvent, useState } from "react";
+// import { useRouter } from "next/navigation";
+// import Image from "next/image";
 
-const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
-  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
-    <Image
-      src={'magnifying-glass.svg'}
-      alt="magnifying-glass"
-      width={40}
-      height={40}
-      className="object-contain"
-    />
-  </button>
-)
+// const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+//   <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+//     <Image
+//       src={'magnifying-glass.svg'}
+//       alt="magnifying-glass"
+//       width={40}
+//       height={40}
+//       className="object-contain"
+//     />
+//   </button>
+// )
 
-export default function SearchBar({ setManufacturer, setModel }: any) {
+// export default function SearchBar({ setManufacturer, setModel }: any) {
 
-  const router = useRouter();
-  const [searchManufacturer, setSearchManufacturer] = useState('');
-  const [searchModel, setSearchModel] = useState('')
+//   const router = useRouter();
+//   const [searchManufacturer, setSearchManufacturer] = useState('');
+//   const [searchModel, setSearchModel] = useState('')
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+//   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
 
-    if (searchManufacturer === '' && searchModel === '') {
-      return alert('검색어를 입력해주세요.')
-    }
+//     if (searchManufacturer === '' && searchModel === '') {
+//       return alert('검색어를 입력해주세요.')
+//     }
 
-    setModel(searchModel);
-    setManufacturer(searchManufacturer);
-  };
-
-
-  return (
-    <form className="searchbar" onSubmit={handleSubmit}>
-      <div className="searchbar__item">
-        <SearchManufacturer
-          selected={searchManufacturer}
-          setSelected={setSearchManufacturer}
-        />
-        <SearchButton otherClasses='sm:hidden' />
-      </div>
-      <div className="searchbar__item">
-        <Image
-          src={'/model-icon.png'}
-          width={25}
-          height={25}
-          className="absolute w-[20px] h-[20px] ml-4"
-          alt="car model"
-        />
-        <input
-          type="text"
-          name="model"
-          value={searchModel}
-          onChange={(e) => setSearchModel(e.target.value)}
-          placeholder="tiguan"
-          className="searchbar__input"
-        />
-        <SearchButton otherClasses='sm:hidden' />
-
-      </div>
-      <SearchButton otherClasses='max-sm:hidden' />
+//     setModel(searchModel);
+//     setManufacturer(searchManufacturer);
+//   };
 
 
-    </form>
-  );
-}
+//   return (
+//     <form className="searchbar" onSubmit={handleSubmit}>
+//       <div className="searchbar__item">
+//         <SearchManufacturer
+//           selected={searchManufacturer}
+//           setSelected={setSearchManufacturer}
+//         />
+//         <SearchButton otherClasses='sm:hidden' />
+//       </div>
+//       <div className="searchbar__item">
+//         <Image
+//           src={'/model-icon.png'}
+//           width={25}
+//           height={25}
+//           className="absolute w-[20px] h-[20px] ml-4"
+//           alt="car model"
+//         />
+//         <input
+//           type="text"
+//           name="model"
+//           value={searchModel}
+//           onChange={(e) => setSearchModel(e.target.value)}
+//           placeholder="tiguan"
+//           className="searchbar__input"
+//         />
+//         <SearchButton otherClasses='sm:hidden' />
+
+//       </div>
+//       <SearchButton otherClasses='max-sm:hidden' />
+
+
+//     </form>
+//   );
+// }
